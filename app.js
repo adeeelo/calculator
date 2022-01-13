776be2c6b3b3ca6ea4626a8e530745b28b5a16cb
adeeloo <am295016@gmail.com>
2022-01-13 16:48:43 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,44 @@
+var display = document.getElementById("displayInput")
+function getButtonValue(btnValue){
+    display.value += btnValue;
+}
+
+function calculate (){
+    var a = display.value;
+    var ans = eval(a);
+    display.value=ans ;
+}
+
+function clearAll (){
+  display.value = "";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
